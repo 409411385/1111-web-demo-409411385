1f19164f1be5852256e9cdeec4a002bf13dfc950
409411385 <409411385@gms.tku.edu.tw>
2022-11-02 07:32:12 +0800

    w08-P1: show fixed navbar with nav items

---
+++ js/demo_85.js
@@ -0,0 +1,21 @@
+function showpage(index) {
+  const p = document.querySelector('.show-classdemo');
+  console.log(p);
+  switch (index) {
+    case 1:
+      p.innerHTML = `<iframe src="./demo/w02/" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      break;
+    case 2:
+      p.innerHTML = `<iframe src="./demo/w03/card_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      break;
+    case 3:
+      p.innerHTML = `<iframe src="./demo/w05/blog_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      break;
+    case 4:
+      p.innerHTML = `<iframe src="./demo/w06/blog_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      break;
+    case 5:
+      p.innerHTML = `<iframe src="./demo/w08/landing_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      break;
+  }
+}
