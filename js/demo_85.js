161d55c9fd160e68d23089a4e035ebaaea1f077b
409411385 <409411385@gms.tku.edu.tw>
2022-11-11 18:53:53 +0800

    W10-P2: show w09-resume from Vercel

--- js/demo_85.js
+++ js/demo_85.js
@@ -21,7 +21,8 @@ function showpage(index) {
       p.innerHTML = `<iframe src="./demo/w08/landing_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
       break;
     case 6:
-      p.innerHTML = `<iframe src="./demo/w09/resume_85.html" style="display:block; border:none; width:100%; height:100%;"></iframe>`
+      p.innerHTML = `<iframe src="./demo/w09/resume_85.html" style="display:block; border:none; width:100%; height:100%;">
+      </iframe>`
       break;
   }
 }
